b81317fc0cb721830c463c92265bd88801ff8a8d
shohuz <shohuz@iuj.ac.jp>
2025-06-07 00:51:09 +0500

    Update hw.py

--- lesson-1/homework-1/hw.py
+++ lesson-1/homework-1/hw.py
@@ -1,2 +1,39 @@
-int('23532523')
-print('Hello World')
+## Homework 1
+#1. Given a side of square. Find its perimeter and area.
+height = int(input('enter height'))
+width = height
+
+p = 2*(height + width)
+p
+height = int(input('enter height'))
+width = height
+
+a = (height * width)
+a
+#2. Given diameter of circle. Find its length.
+diameter = float(input('enter diameter'))
+math_pi = 3.14159
+l = math_pi * diameter
+l
+#3. Given two numbers a and b. Find their mean.
+a = float(input('enter number'))
+b = float(input('enter number'))
+data = [a, b]
+import statistics
+m = statistics.mean(data)
+m
+#4. Given two numbers a and b. Find their sum, product and square of each number.
+a = float(input('enter number'))
+b = float(input('enter number'))
+sum = a + b
+sum
+a = float(input('enter number'))
+b = float(input('enter number'))
+product = a * b
+product
+a = float(input('enter number'))
+square = a**2
+square
+b = float(input('enter number'))
+square = b**2
+square
